6d5da9f3417d5b42de7147daa7835f0994af73df
Ree9o <ree9.business@gmail.com>
2024-02-15 21:56:35 +0900

    型の抽出・カスタムフックの抽出に合わせた調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Day from "./components/elements/Day";
+import { useWeatherData } from "./hooks/useWeatherData";
 
 function App() {
+  const { weatherData } = useWeatherData();
   return (
     <>
       <div className="w-screen bg-gradient-to-br from-slate-50 to-indigo-100">
         <div className="grid grid-cols-2 gap-3 p-16">
-          <Day />
-          <Day />
+          <Day data={weatherData} />
         </div>
       </div>
     </>
